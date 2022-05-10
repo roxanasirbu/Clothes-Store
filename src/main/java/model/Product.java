58b3ee90334241dd7538d80af04fc514fdda4800
Roxana Sirbu <roxana.sirbu@student.upt.ro>
2022-05-10 16:13:41 +0300

    add code for order and product

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,4 +1,51 @@
 package model;
 import app.MainApplication;
+
 public class Product {
+    private  int id;
+    private String name;
+    private String desc;
+    private double price;
+    private int size;
+
+
+    public Product( String name, int size , double price,  String desc) {
+
+        this.name = name;
+        this.desc = desc;
+        this.price = price;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
