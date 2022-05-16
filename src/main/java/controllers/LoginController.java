2b3a59c6bf8dba092a506eec49c22efde136fdb0
Roxana Sirbu <roxana.sirbu@student.upt.ro>
2022-05-16 20:30:33 +0300

    change the type of password and add the logo photo on login page

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -2,14 +2,17 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,7 +23,11 @@ import java.util.logging.Logger;
 import app.MainApplication;
 import manager.ConnectionUtil;
 
-public class LoginController {
+
+public class LoginController implements Initializable {
+    @FXML
+    private ImageView orderImageView;
+
     MainApplication main;
     Stage primaryStage;
     Connection con = null;
@@ -102,5 +109,14 @@ public class LoginController {
     public void setmain(MainApplication mainApplication, Stage primaryStage) {
         this.main = mainApplication;
         this.primaryStage =primaryStage;
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        File orderFile = new File("image/logo.jpeg");
+        Image orderImage= new Image(orderFile.toURI().toString());
+        orderImageView.setImage(orderImage);
+
+
     }
 }
