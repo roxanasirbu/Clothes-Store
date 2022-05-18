580f9c4710454aa2c6ecb645b594008fb27d6cf0
Roxana Sirbu <roxana.sirbu@student.upt.ro>
2022-05-18 16:39:15 +0300

    add on this branch the code for db connection

--- src/main/java/manager/ConnectionUtil.java
+++ src/main/java/manager/ConnectionUtil.java
@@ -5,6 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionUtil {
-
+    Connection conn = null;
+    public static Connection conDB(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/pos","root", "");
+            return conn;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+    }
 }
 
