b2915a178f8643ff2fa358c619b0aafc59d26a53
Sirbu Roxana-Ionela <80626056+roxanasirbu@users.noreply.github.com>
2022-05-20 09:55:59 +0300

    delete

--- src/main/java/app/MainApplication.java
+++ src/main/java/app/MainApplication.java
@@ -1,19 +1,1 @@
-package app;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import controllers.LoginController;
-import java.io.IOException;
-import controllers.RegisterController;
-import controllers.AdminController;
-import controllers.UserController;
 
-public class MainApplication extends Application {
-
-
-
-
-
-}
