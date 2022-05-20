042bb0465e71bfd1f1e85bf4bb389b37b248320e
Grecu Denisa <deniii.denisa28@yahoo.com>
2022-05-20 10:08:32 +0300

    code for OrderController

--- src/main/java/controllers/OrderController.java
+++ src/main/java/controllers/OrderController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import app.MainApplication;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import manager.ConnectionUtil;
 
 import java.sql.Connection;
 import java.sql.Statement;
